cb6e0ace0d350fb8437c59706698179fe1f361b1
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-06-29 17:45:18 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,7 +3,7 @@ def meal_choice (veg1, veg2, protein="tofu")
   a_plate_of ="A plate of #{protein} with #{veg1} and #{veg2}."
   meal_choice_return= nutritious_meal + a_plate_of
   puts a_plate_of
-  return a_plate_of
+  a_plate_of
 end
 
 meal_choice "carrots","string beans","steak"
